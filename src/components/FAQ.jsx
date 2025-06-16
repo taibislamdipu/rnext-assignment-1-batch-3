bb23c9b40d45f07e3c74a1fedf1d2340783081c7
taibislamdipu <mailtaibislam@gmail.com>
2025-06-16 13:25:25 +0600

    new

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,6 +1,138 @@
 import ArrowUp from "../icons/ArrowUp";
+import DownArrow from "../icons/DownArrow";
 
 export default function FAQ() {
+  const faqs = [
+    {
+      id: 1,
+      question: "Can I use Landwind in open-source projects?",
+      answer: (
+        <>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Landwind is an open-source library of interactive components built
+            on top of Tailwind CSS including buttons, dropdowns, modals,
+            navbars, and more.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Check out this guide to learn how to{" "}
+            <a
+              href="#"
+              className="text-purple-600 hover:underline dark:text-purple-500"
+            >
+              get started
+            </a>{" "}
+            and start developing websites even faster with components on top of
+            Tailwind CSS.
+          </p>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      question: "Is there a Figma file available?",
+      answer: (
+        <>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Landwind is first conceptualized and designed using the Figma
+            software so everything you see in the library has a design
+            equivalent in our Figma file.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Check out the{" "}
+            <a
+              href="#"
+              className="text-purple-600 hover:underline dark:text-purple-500"
+            >
+              Figma design system
+            </a>{" "}
+            based on the utility classes from Tailwind CSS and components from
+            Landwind.
+          </p>
+        </>
+      ),
+    },
+    {
+      id: 3,
+      question: "What are the differences between Landwind and Tailwind UI?",
+      answer: (
+        <>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            The main difference is that the core components from Landwind are
+            open source under the MIT license, whereas Tailwind UI is a paid
+            product. Another difference is that Landwind relies on smaller and
+            standalone components, whereas Tailwind UI offers sections of pages.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            However, we actually recommend using both Landwind, Landwind Pro,
+            and even Tailwind UI as there is no technical reason stopping you
+            from using the best of two worlds.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Learn more about these technologies:
+          </p>
+          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+            <li>
+              <a
+                href="#"
+                className="text-purple-600 hover:underline dark:text-purple-500"
+              >
+                Landwind Pro
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-purple-600 hover:underline dark:text-purple-500"
+              >
+                Tailwind UI
+              </a>
+            </li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      id: 4,
+      question: "What about browser support?",
+      answer: (
+        <>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            The main difference is that the core components from Landwind are
+            open source under the MIT license, whereas Tailwind UI is a paid
+            product. Another difference is that Landwind relies on smaller and
+            standalone components, whereas Tailwind UI offers sections of pages.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            However, we actually recommend using both Landwind, Landwind Pro,
+            and even Tailwind UI as there is no technical reason stopping you
+            from using the best of two worlds.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Learn more about these technologies:
+          </p>
+          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+            <li>
+              <a
+                href="#"
+                className="text-purple-600 hover:underline dark:text-purple-500"
+              >
+                Landwind Pro
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-purple-600 hover:underline dark:text-purple-500"
+              >
+                Tailwind UI
+              </a>
+            </li>
+          </ul>
+        </>
+      ),
+    },
+  ];
+
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="mx-auto max-w-screen-xl px-4 pb-8 lg:px-6 lg:pb-24">
@@ -8,228 +140,32 @@ export default function FAQ() {
           Frequently asked questions
         </h2>
         <div className="mx-auto max-w-screen-md">
-          <div
-            id="accordion-flush"
-            data-accordion="collapse"
-            data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
-            data-inactive-classes="text-gray-500 dark:text-gray-400"
-          >
-            <h3 id="accordion-flush-heading-1">
-              <button
-                type="button"
-                className="flex w-full items-center justify-between border-b border-gray-200 bg-white py-5 text-left font-medium text-gray-900 dark:border-gray-700 dark:bg-gray-900 dark:text-white"
-                data-accordion-target="#accordion-flush-body-1"
-                aria-expanded="true"
-                aria-controls="accordion-flush-body-1"
-              >
-                <span>Can I use Landwind in open-source projects?</span>
-                <ArrowUp />
-              </button>
-            </h3>
-            <div
-              id="accordion-flush-body-1"
-              className=""
-              aria-labelledby="accordion-flush-heading-1"
-            >
-              <div className="border-b border-gray-200 py-5 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Landwind is an open-source library of interactive components
-                  built on top of Tailwind CSS including buttons, dropdowns,
-                  modals, navbars, and more.
-                </p>
-                <p className="text-gray-500 dark:text-gray-400">
-                  Check out this guide to learn how to{" "}
-                  <a
-                    href="#"
-                    className="text-purple-600 hover:underline dark:text-purple-500"
+          <div id="accordion-flush">
+            {faqs.map((faq, index) => (
+              <div key={faq.id}>
+                <h3 id={`accordion-flush-heading-${faq.id}`}>
+                  <button
+                    type="button"
+                    className="flex w-full items-center justify-between border-b border-gray-200 py-5 text-left font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400"
+                    data-accordion-target={`#accordion-flush-body-${faq.id}`}
+                    aria-expanded={index === 0 ? "true" : "false"}
+                    aria-controls={`accordion-flush-body-${faq.id}`}
                   >
-                    get started
-                  </a>{" "}
-                  and start developing websites even faster with components on
-                  top of Tailwind CSS.
-                </p>
-              </div>
-            </div>
-            <h3 id="accordion-flush-heading-2">
-              <button
-                type="button"
-                className="flex w-full items-center justify-between border-b border-gray-200 py-5 text-left font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400"
-                data-accordion-target="#accordion-flush-body-2"
-                aria-expanded="false"
-                aria-controls="accordion-flush-body-2"
-              >
-                <span>Is there a Figma file available?</span>
-                <svg
-                  data-accordion-icon=""
-                  className="h-6 w-6 shrink-0"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                    <span>{faq.question}</span>
+                    {index === 0 ? <ArrowUp /> : <DownArrow />}
+                  </button>
+                </h3>
+                <div
+                  id={`accordion-flush-body-${faq.id}`}
+                  className={index === 0 ? "" : "hidden"}
+                  aria-labelledby={`accordion-flush-heading-${faq.id}`}
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </h3>
-            <div
-              id="accordion-flush-body-2"
-              className="hidden"
-              aria-labelledby="accordion-flush-heading-2"
-            >
-              <div className="border-b border-gray-200 py-5 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Landwind is first conceptualized and designed using the Figma
-                  software so everything you see in the library has a design
-                  equivalent in our Figma file.
-                </p>
-                <p className="text-gray-500 dark:text-gray-400">
-                  Check out the{" "}
-                  <a
-                    href="#"
-                    className="text-purple-600 hover:underline dark:text-purple-500"
-                  >
-                    Figma design system
-                  </a>
-                  based on the utility classes from Tailwind CSS and components
-                  from Landwind.
-                </p>
+                  <div className="border-b border-gray-200 py-5 dark:border-gray-700">
+                    {faq.answer}
+                  </div>
+                </div>
               </div>
-            </div>
-            <h3 id="accordion-flush-heading-3">
-              <button
-                type="button"
-                className="flex w-full items-center justify-between border-b border-gray-200 py-5 text-left font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400"
-                data-accordion-target="#accordion-flush-body-3"
-                aria-expanded="false"
-                aria-controls="accordion-flush-body-3"
-              >
-                <span>
-                  What are the differences between Landwind and Tailwind UI?
-                </span>
-                <svg
-                  data-accordion-icon=""
-                  className="h-6 w-6 shrink-0"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </h3>
-            <div
-              id="accordion-flush-body-3"
-              className="hidden"
-              aria-labelledby="accordion-flush-heading-3"
-            >
-              <div className="border-b border-gray-200 py-5 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  The main difference is that the core components from Landwind
-                  are open source under the MIT license, whereas Tailwind UI is
-                  a paid product. Another difference is that Landwind relies on
-                  smaller and standalone components, whereas Tailwind UI offers
-                  sections of pages.
-                </p>
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  However, we actually recommend using both Landwind, Landwind
-                  Pro, and even Tailwind UI as there is no technical reason
-                  stopping you from using the best of two worlds.
-                </p>
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Learn more about these technologies:
-                </p>
-                <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                  <li>
-                    <a
-                      href="#"
-                      className="text-purple-600 hover:underline dark:text-purple-500"
-                    >
-                      Landwind Pro
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="text-purple-600 hover:underline dark:text-purple-500"
-                    >
-                      Tailwind UI
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <h3 id="accordion-flush-heading-4">
-              <button
-                type="button"
-                className="flex w-full items-center justify-between border-b border-gray-200 py-5 text-left font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400"
-                data-accordion-target="#accordion-flush-body-4"
-                aria-expanded="false"
-                aria-controls="accordion-flush-body-4"
-              >
-                <span>What about browser support?</span>
-                <svg
-                  data-accordion-icon=""
-                  className="h-6 w-6 shrink-0"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </h3>
-            <div
-              id="accordion-flush-body-4"
-              className="hidden"
-              aria-labelledby="accordion-flush-heading-4"
-            >
-              <div className="border-b border-gray-200 py-5 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  The main difference is that the core components from Landwind
-                  are open source under the MIT license, whereas Tailwind UI is
-                  a paid product. Another difference is that Landwind relies on
-                  smaller and standalone components, whereas Tailwind UI offers
-                  sections of pages.
-                </p>
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  However, we actually recommend using both Landwind, Landwind
-                  Pro, and even Tailwind UI as there is no technical reason
-                  stopping you from using the best of two worlds.
-                </p>
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Learn more about these technologies:
-                </p>
-                <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                  <li>
-                    <a
-                      href="#"
-                      className="text-purple-600 hover:underline dark:text-purple-500"
-                    >
-                      Landwind Pro
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="text-purple-600 hover:underline dark:text-purple-500"
-                    >
-                      Tailwind UI
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
